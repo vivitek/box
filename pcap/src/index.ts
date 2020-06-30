1f1725c5cb9e61d9509b2c1235c35458d67addc1
Arthur Walsh <arthur.walsh@epitech.eu>
2020-06-30 20:39:44 +0200

    clean logging on pcap

--- pcap/src/index.ts
+++ pcap/src/index.ts
@@ -1,10 +1,12 @@
 import MsgMgr from "./msgMgr/msgMgr";
 import * as pcap from 'pcap';
 import * as os from 'os';
+import { Logger } from "./Logger/Logger";
 
 const netinter = os.networkInterfaces();
 
 const msgMgr = new MsgMgr(process.env.RABBITMQ_URL, 'pcap');
+const logger = new Logger('./pcap.stdout', './pcap.stderr');
 
 async function initMsgMgr() {
     await msgMgr.connect();
@@ -19,19 +21,19 @@ tcpTracker.on('session', session => {
     const dst = String(session.dst);
     const src_ip = src.substring(0, src.indexOf(':'));
     const dst_ip = dst.substring(0, dst.indexOf(':'));
-    console.log(`from ${src_ip} to ${dst_ip}`);
+    logger.info(`from ${src_ip} to ${dst_ip}`);
     msgMgr.sendMsg(`from ${src_ip} to ${dst_ip}`);
     session.on('end', s => {
-        console.info('[-] - End of session');
+        logger.info('[-] - End of session');
     });
 })
 
 pcapSession.on('packet', raw => {
     const packet = pcap.decode.packet(raw);
     if (process.argv[2] && (process.argv[2] == '--details' || process.argv[2] == '-d')) {
-        console.log("Received packet:", packet);
-        console.log(`saddr: ${packet.payload.payload.saddr}`);
-        console.log(`daddr: ${packet.payload.payload.daddr}`);
+        logger.info("Received packet:", packet);
+        logger.info(`saddr: ${packet.payload.payload.saddr}`);
+        logger.info(`daddr: ${packet.payload.payload.daddr}`);
         process.exit(0);
     } else {
         tcpTracker.track_packet(packet);
