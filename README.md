c54c1fba74bd2323c8b82bfb25fa038a5c2e5df5
Arthur Walsh <walsh.arthur@outlook.com>
2020-09-03 15:11:24 +0200

    BalenaCloud building project

--- README.md
+++ README.md
@@ -15,3 +15,9 @@ See [contributing guidelines](./holocron/router/contributing.md)
 In local, project can be developped in a container using VSCode Dev Container functionnality.
 
 Required for container dev: Docker CE/CC, VSCode, Docker VSCode Extension, Remote - Containers Extension
+
+### Building
+
+As the project need to be built for an arm architecture (aarch64), the TS files will be built locally, then sent and built on BalenaCloud platform, and pushed to devices. The devices will automatically be updated if the build succeeds.
+
+Use `./build.sh` to build and publish project
--- build.sh
+++ build.sh
@@ -5,4 +5,32 @@
 
 #!/bin/sh
 
-if 
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+NC='\033[0m'
+
+echo -e "${ORANGE}## BUILDING PROJECT FOR BALENA OS ${NC}##"
+
+# Building typescript containers
+containers="container"
+for container in containers; do
+  cd container
+  yarn build
+  if [ $? -eq 1 ]; then
+    echo -e "${RED}XXX ERROR BUILDING TS IN ${ORANGE}${container}${RED} XXX${NC}"
+    exit 1
+  fi
+  cd ..
+done
+echo -e "${GREEN}[][][] Done ! [][][]${NC}"
+
+# Building on Balena cloud
+echo -e "${ORANGE}Building containers on BalenaCloud${NC}"
+
+balena push VincipitRouter
+if [ $? -eq 1 ]; then
+  echo -e "${RED}XXX ERROR BUILDING CONTAINER ON BALENACLOUD XXX${NC}"
+  exit 1
+fi
+echo -e "${GREEN}[][][] Done ! [][][]${NC}"
