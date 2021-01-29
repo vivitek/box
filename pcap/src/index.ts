4e70f47936a85f479e943fbf21b29cef17dce62c
Arthur Walsh <walsh.arthur@outlook.com>
2021-01-29 21:01:17 +0100

    Pcap reading packets + pushing to RMQ
    read whatsapp + discord + fb target packets

--- pcap/src/index.ts
+++ pcap/src/index.ts
@@ -1,20 +1,53 @@
-// import MsgMgr from '../../utils/msgMgr/msgMgr'
+import MsgMgr from '../../utils/msgMgr/msgMgr'
 import { Logger } from '../../utils/Logger/Logger'
 import * as pcap from 'pcap'
+import { networkInterfaces } from 'os'
+
+const msgMgr = new MsgMgr(process.env.RABBITMQ_URL || '', 'pcap_push')
+msgMgr.connect()
+
+function selectNetworkInterface(): string {
+  const netInterfaces = Object.keys(networkInterfaces())
+  if (netInterfaces[0] !== 'lo') return netInterfaces[0]
+  return netInterfaces[1]
+}
+
+enum PCAPTYPE {
+  REQUEST,
+  RESPONSE,
+  PUSH,
+}
+
+type PcapPacket = {
+  source: string
+  destination: string
+  requestIsis: boolean
+  service: string | undefined
+  type: PCAPTYPE
+}
 
 export function init(): void {
   const logger = new Logger('./msgbroker.stdout', './msgbroker.stderr')
   const tcpTracker = new pcap.TCPTracker()
-  const pcapSession = pcap.createSession('wlan0')
+  const pcapSession = pcap.createSession(selectNetworkInterface())
 
   tcpTracker.on('session', (session: any) => {
     const src = String(session.src)
     const dst = String(session.dst)
     const src_ip = src.substring(0, src.indexOf(':'))
     const dst_ip = dst.substring(0, dst.indexOf(':'))
-    logger.info(`from ${src_ip} to ${dst_ip}`)
 
-    // msgMgr.sendMsg(`from ${src_ip} to ${dst_ip}`);
+    const packet: PcapPacket = {
+      source: src_ip,
+      destination: dst_ip,
+      requestIsis: true,
+      service: undefined,
+      type: PCAPTYPE.REQUEST,
+    }
+    console.info('PACKET -', packet)
+
+    msgMgr.sendMsg(JSON.stringify(packet))
+
     session.on('end', (_s: any) => {
       logger.info('[-] - End of session')
     })
