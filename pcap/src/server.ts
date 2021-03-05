0391ef8cb8c00db4e48f0cc7c8c75fb5ac55aa8e
Matteo Gassend <matteo.gassend@epitech.eu>
2021-03-05 16:25:47 +0100

    [add] pcap converted to typescript

---
+++ pcap/src/server.ts
@@ -0,0 +1,71 @@
+import * as pcap from "pcap";
+import * as dns from "dns";
+import { exit } from "process";
+import * as amqplib from "amqplib";
+
+let channel: amqplib.Channel;
+
+// RABBITMQ INIT
+const initRabbitMQ = async () => {
+  try {
+    const connection = await amqplib.connect({
+      hostname: "0.0.0.0",
+      username: "vivi",
+      password: "vivitek",
+    });
+    channel = await connection.createChannel();
+    await channel.assertQueue("pcap");
+  } catch (error) {
+    exit(1);
+  }
+};
+
+const sendToQueue = async (data) => {
+  await channel.sendToQueue("pcap", Buffer.from(JSON.stringify(data)));
+};
+
+// PCAP INIT
+
+const initPcap = async () => {
+  const pcap_session: pcap.PcapSession = pcap.createSession("", {
+    filter: "ip proto \\tcp",
+  });
+  const tcpTracker = new pcap.TCPTracker();
+
+  tcpTracker.on("session", async (session) => {
+    console.log(
+      `Start of session between ${session.src_name} and ${session.dst_name}`
+    );
+    sendToQueue(session);
+    session.on("end", async (session) => {
+      try {
+        const domains = await dns.promises.reverse(session.dst.split(":")[0]);
+        console.log(
+          `End of session ${session.src_name} and ${session.dst_name}`
+        );
+        console.log(
+          `Sent ${session.send_bytes_payload} bytes and received ${session.recv_bytes_payload} bytes`
+        );
+        console.log(domains);
+      } catch (error) {
+        const domains = [];
+        console.log(
+          `End of session ${session.src_name} and ${session.dst_name}`
+        );
+        console.log(
+          `Sent ${session.send_bytes_payload} bytes and received ${session.recv_bytes_payload} bytes`
+        );
+        console.log(domains);
+      }
+    });
+  });
+
+  pcap_session.on("packet", (raw_packet) => {
+    const packet = pcap.decode.packet(raw_packet);
+    tcpTracker.track_packet(packet);
+  });
+};
+
+initRabbitMQ().then(() => {
+  initPcap();
+});
