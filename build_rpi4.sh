1c7dadf5fe14409ed48ce6c4c2b3eb4c12880e1f
Matteo Gassend <matteo.gassend@epitech.eu>
2021-03-06 11:01:37 +0100

    [fix] added debug flag to rpi4 build

--- build_rpi4.sh
+++ build_rpi4.sh
@@ -1,4 +1,4 @@
  #!/usr/bin/env sh
 
 echo -e "Building containers in emulated containers"
-balena build --deviceType raspberrypi4-64 --arch aarch64 --emulated
+balena build --deviceType raspberrypi3-64 --arch aarch64 --emulated --debug
