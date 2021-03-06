e2170aecdec210dce8b4d1215c9d70ce2abf16a6
Matteo Gassend <matteo.gassend@epitech.eu>
2021-03-06 14:48:29 +0100

    [fix] added sudo to build command

--- build_rpi4.sh
+++ build_rpi4.sh
@@ -1,4 +1,4 @@
  #!/usr/bin/env sh
 
 echo -e "Building containers in emulated containers"
-balena build --deviceType raspberrypi3-64 --arch aarch64 --emulated --debug
+sudo balena build --deviceType raspberrypi3-64 --arch aarch64 --emulated --debug
