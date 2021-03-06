21f197a6d12c0f7194fafbd2730692a64a93ebf7
Matteo Gassend <matteo.gassend@epitech.eu>
2021-03-06 15:04:06 +0100

    [fix] removed sudo from build script

--- build_rpi4.sh
+++ build_rpi4.sh
@@ -1,4 +1,4 @@
  #!/usr/bin/env sh
 
 echo -e "Building containers in emulated containers"
-sudo balena build --deviceType raspberrypi3-64 --arch aarch64 --emulated --debug
+balena build --deviceType raspberrypi3-64 --arch aarch64 --emulated --debug
