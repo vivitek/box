2f9d3c536aa67b39b49afc2650ebb87e41c2a34a
Matteo Gassend <matteo.gassend@epitech.eu>
2021-03-10 08:59:18 +0100

    [fix] build_rpi4 actually builds on an emulated rpi4 now

--- build_rpi4.sh
+++ build_rpi4.sh
@@ -1,4 +1,4 @@
  #!/usr/bin/env sh
 
 echo -e "Building containers in emulated containers for rpi4"
-balena build --deviceType raspberrypi3-64 --arch aarch64 --emulated -m
+balena build --deviceType raspberrypi4-64 --arch aarch64 --emulated -m
