2cf930772eadd708eed6fe6609855a5510b62a39
Matteo Gassend <matteo.gassend@epitech.eu>
2021-05-13 13:59:33 +0200

    [add] created install script for hotspot

---
+++ ov_install.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m'
+
+echo -e "Welcome to OpenViVi! Now commencing installation"
+
+echo -e "${GREEN}Configuring Hotspot network${NC}"
+sudo cp config/10-my-config.yml /etc/netplan
+sudo netplan generate
+sudo netplan apply
+echo -e "${GREEN}Hotspot configured!${NC}"
+
