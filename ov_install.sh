4fb70042c6d18289ba01ec613e196acd89cee914
Matteo Gassend <matteo.gassend@epitech.eu>
2021-05-13 14:05:40 +0200

    [fix] fixing typos

--- ov_install.sh
+++ ov_install.sh
@@ -6,7 +6,7 @@ NC='\033[0m'
 echo -e "Welcome to OpenViVi! Now commencing installation"
 
 echo -e "${GREEN}Configuring Hotspot network${NC}"
-sudo cp config/10-my-config.yml /etc/netplan
+sudo cp configs/10-my-config.yml /etc/netplan
 sudo netplan generate
 sudo netplan apply
 echo -e "${GREEN}Hotspot configured!${NC}"
