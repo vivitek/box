407e919272f9c5889b1acb45340bad4fca34e437
Matteo Gassend <matteo.gassend@epitech.eu>
2021-05-18 20:07:47 +0200

    [fix] hammer to fall

--- ov_install.sh
+++ ov_install.sh
@@ -87,7 +87,7 @@ cd ..
 echo "${GREEN}Installing DHCP dependencies${NC}"
 cd dhcpd
 npm install
-./node_modules/typescript/bin/tsc 
+./node_modules/typescript/bin/tsc
 echo "${GREEN}Starting DHCP service${NC}"
 sudo pm2 start --name dhcp dist/main.js
 cd ..
@@ -97,7 +97,7 @@ cd ..
 echo "${GREEN}Installing GraphQl dependencies${NC}"
 cd graphql
 npm install
-./node_modules/typescript/bin/tsc 
+./node_modules/typescript/bin/tsc
 echo "${GREEN}Starting GraphQl service${NC}"
 sudo pm2 start --name graphql dist/index.js
 cd ..
