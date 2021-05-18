ada520ba7544f72c6ef135148cce90f332d99e27
Matteo Gassend <matteo.gassend@epitech.eu>
2021-05-18 18:47:38 +0200

    [add] dropping docker

--- ov_install.sh
+++ ov_install.sh
@@ -5,23 +5,23 @@ NC='\033[0m'
 
 echo "Welcome to OpenVVRT! Now commencing installation"
 
-#Installing docker
-echo "${GREEN}Removing existing Docker installation${NC}"
-sudo apt remove -y docker docker-engine docker.io containerd runc
-echo "${GREEN}Installing Docker dependencies${NC}"
-sudo apt install -y apt-transport-https ca-certificates curl gnupg lsb-release
-echo "${GREEN}Adding Docker GPT key${NC}"
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-echo "${GREEN}Adding Docker repository${NC}"
-echo "deb [arch=arm64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt update
-echo "${GREEN}Installing Docker${NC}"
-sudo apt install -y docker-ce docker-ce-cli containerd.io
-echo "${GREEN}Installing docker-compose and seeting permissions${NC}"
-sudo apt install -y python3-pip
-sudo pip3 install docker-compose
+# # Installing docker
+# echo "${GREEN}Removing existing Docker installation${NC}"
+# sudo apt remove -y docker docker-engine docker.io containerd runc
+# echo "${GREEN}Installing Docker dependencies${NC}"
+# sudo apt install -y apt-transport-https ca-certificates curl gnupg lsb-release
+# echo "${GREEN}Adding Docker GPT key${NC}"
+# curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+# echo "${GREEN}Adding Docker repository${NC}"
+# echo "deb [arch=arm64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+# sudo apt update
+# echo "${GREEN}Installing Docker${NC}"
+# sudo apt install -y docker-ce docker-ce-cli containerd.io
+# echo "${GREEN}Installing docker-compose and seeting permissions${NC}"
+# sudo apt install -y python3-pip
+# sudo pip3 install docker-compose
 
-#Installing nvm and recommended version
+# Installing nvm and recommended version
 echo "${GREEN}Installing and configuring nodejs${NC}"
 curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash -
 sudo apt install -y nodejs
@@ -31,20 +31,19 @@ sudo npm i -g pm2@latest
 pm2 kill
 pm2 startup
 
-#Configuring openvvrt api
-echo "${GREEN}Installing ov_api dependencies${NC}"
+# Configuring openvvrt api
+echo "${GREEN}Installing and activating API${NC}"
 cd ov_api
 npm install
 sudo pm2 start --name ov-api index.js
 cd ..
 
-echo "${GREEN}Building Dockerized services${NC}"
-sudo docker-compose build dhcpd graphql
+echo "${GREEN}Building baremetal services${NC}"
 
 
-#Configuring RabbitMQ
-echo "${GREEN}Installing RabbitMQ${NC}"
-sudo apt install -y erlang rabbitmq-server 
+# Configuring RabbitMQ
+echo "${GREEN}Building RabbitMQ ${NC}"
+sudo apt install -y rabbitmq-server
 sudo systemctl enable rabbitmq-server
 sudo systemctl start rabbitmq-server
 echo "${GREEN}Activating rabbitmq management plugin${NC}"
@@ -54,25 +53,53 @@ sudo rabbitmqctl add_user vivi vivitek
 sudo rabbitmqctl set_user_tags vivi administrator
 sudo rabbitmqctl set_permissions -p / vivi ".*" ".*" ".*"
 
-#Configuring postgres
-echo "${GREEN}Installing OpenVVRT's services${NC}"
-sudo docker-compose up -d postgres
 
+echo "${GREEN}Installing Postgresql services${NC}"
+sudo apt install -y postgresql postgresql-contrib
+sudo -u postgres psql postgres < ./postgres/init.sql
+
+# Configuring postgres
+echo "${GREEN}Installing OpenVVRT's services${NC}"
+
+
+# Configuring firewall
 echo "${GREEN}Installing Firewall service dependencies${NC}"
 sudo apt install -y build-essential libpq-dev procps nftables
+echo "${GREEN}Creating firewall systemd${NC}"
+FIREWALL_SYSTEMD=${cat ./configs/firewall.service.template | sed 's@$PWD@'"$PWD"'@'}
+echo $FIREWALL_SYSTEMD
+sudo echo $FIREWALL_SYSTEMD > /lib/systemd/system/firewall.service
 echo "${GREEN}Installing Firewall service${NC}"
 cd firewall
 sudo pip3 install -r requirements.txt
-
-psql postgresql://fire:fire2020@localhost:5432/postgres < ../postgres/init.sql
-
+echo "${GREEN}Starting firewall service${NC}"
 python3 manage.py db init
 python3 manage.py db migrate
 python3 manage.py db upgrade
+echo "${GREEN}Starting firewall systemd${NC}"
+sudo systemctl enable firewall
+sudo systemctl start firewall
 cd ..
 
 
-sudo docker-compose up -d dhcpd graphql
+# Configuring DHCP
+echo "${GREEN}Installing DHCP dependencies${NC}"
+cd dhcpd
+npm install
+./node_modules/typescript/tsc
+echo "${GREEN}Starting DHCP service${NC}"
+sudo pm2 start --name dhcp dist/main.js
+cd ..
+
+
+# Configuring GraphQL relay
+echo "${GREEN}Installing GraphQl dependencies${NC}"
+cd graphql
+npm install
+./node_modules/typescript/tsc
+echo "${GREEN}Starting GraphQl service${NC}"
+sudo pm2 start --name graphql dist/index.js
+cd ..
 
 
 #Network hotspot configuration
@@ -84,4 +111,4 @@ echo "${GREEN}Hotspot configured!${NC}"
 
 
 sudo apt autoremove -y
-sudo python3 firewall/manage.py run
+sudo systemctl --follow
