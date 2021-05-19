bdf0a70bb40da25153cdfdf0ff9f1f3e93dafc54
thmarinho <theo.marinho@epitech.eu>
2021-05-19 14:39:18 +0200

    [fix] tous les cris les sos

--- ov_install.sh
+++ ov_install.sh
@@ -37,7 +37,7 @@ sudo -E apt install -y python3-pip
 
 # Installing nvm and recommended version
 echo "${GREEN}Installing and configuring nodejs${NC}"
-curl -sL https://deb.nodesource.com/setup_18.x | sudo -E bash -
+curl -sL https://deb.nodesource.com/setup_16.x | sudo bash -
 sudo -E apt install -y nodejs
 
 echo "${GREEN}Installing pm2${NC}"
