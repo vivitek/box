8294332538ab34f44817bee59b2f1fb323e5b2b1
Remi Cabande <remi.cabande@epitech.eu>
2021-05-27 13:11:27 +0200

    [del] actually remove PyIP folder ...

--- firewall/PyIP/old/v2/Tools.py
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/python3
-
-def find_between( s, first, last ):
-    try:
-        start = s.index( first ) + len( first )
-        end = s.index( last, start )
-        return s[start:end]
-    except ValueError:
-        return ""
