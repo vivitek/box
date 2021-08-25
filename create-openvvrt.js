2ce26eb13aeb7016dec127601bfc97f6c7b08586
Theo Marinho <theo.marinho@epitech.eu>
2021-08-25 16:54:10 +0200

    [fix] fix spinners' name

--- create-openvvrt.js
+++ create-openvvrt.js
@@ -7,19 +7,20 @@ const config = require('./config/openvvrt.config.json')
 const log = openSync(`${+new Date()}.log`)
 const spinnies = new Spinnies()
 
-const runCommands = async (commands) => {
+const runCommands = async (name, commands) => {
   if (!commands || !commands.length)
     return
   commands.forEach(command => {
     try {
-      spinnies.add(command)
+      spinnies.add(name)
       const { stdout, stderr } = await execa.command(command)
       if (stderr && stderr.length)
         throw `Could not run ${command}`
       writeSync(log, stdout)
+      spinnies.success(name)
       console.log(stdout)
     } catch (err) {
-      spinnies.fail(`Could not run ${command}`)
+      spinnies.fail(name)
       writeSync(log, err)
       console.log(chalk.red(err))
       process.exit(1)
@@ -34,14 +35,14 @@ const start = async () => {
     console.log(chalk.bold('Installing dependencies'))
     await runCommands([`sudo apt install -y ${config.dependencies.join(' ')}`])
 
-    console.log(chalk.bold('Initialzing services'))
+    console.log(chalk.bold('Initializing services'))
     await Promise.all(config.services.map(async service => {
       spinnies.add(service.name)
       if (service.dependencies && service.dependencies.length)
-        await runCommands([`sudo apt install -y ${service.dependencies}`])
-      await runCommands(service.preInstallCmd)
-      await runCommands(`cd ${service.folder} && ${service.installCmd.join(' && ')}`)
-      await runCommands(`cd ${service.folder} && ${service.runCmd.join(' && ')}`)
+        await runCommands("Install dependencies", [`sudo apt install -y ${service.dependencies.join(' ')}`])
+      await runCommands("Run pre-install commands", service.preInstallCmd)
+      await runCommands("Run install commands", `cd ${service.folder} && ${service.installCmd.join(' && ')}`)
+      await runCommands("Start service", `cd ${service.folder} && ${service.runCmd.join(' && ')}`)
       spinnies.success(service.name)
     }))
   } catch (err) {
