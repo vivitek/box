3ff74143131249eddf7e97ed39f35fefc05351a9
thmarinho <theo.marinho@epitech.eu>
2021-08-25 17:49:57 +0000

    [add] add execPath on command execution

--- create-openvvrt.js
+++ create-openvvrt.js
@@ -7,14 +7,14 @@ const config = require('./config/openvvrt.config.json')
 const log = openSync(`${+new Date()}.log`, "a+")
 const spinnies = new Spinnies()
 
-const runCommands = async (name, commands, path = '.') => {
+const runCommands = async (name, commands, execPath = '.') => {
   if (!commands || commands.length === 0)
     return
  await Promise.all(commands.map(async command => {
     try {
       spinnies.add(name)
-	   console.log(process.cwd())
-      const { stdout, stderr } = await execa.command(command)
+      const { stdout, stderr } = await execa.command(command, {execPath})
+	    console.log(stderr)
       if (stderr && !stderr.startsWith("\nWARNING"))
         throw stderr
       writeSync(log, stdout)
@@ -39,12 +39,13 @@ const start = async () => {
     console.log(chalk.bold('Initializing services'))
     await Promise.all(config.services.map(async service => {
       spinnies.add(service.name)
-      if (service.dependencies && service.dependencies.length)
-        await runCommands("Install dependencies", [`sudo apt install -y ${service.dependencies.join(' ')}`])
-      await runCommands("Run pre-install commands", service.preInstallCmd)
-      await runCommands("Run install commands", [`cd ${service.folder} && ${service.installCmd.join(' && ')}`])
-      await runCommands("Start service", [`cd ${service.folder} && ${service.runCmd.join(' && ')}`])
+      //if (service.dependencies && service.dependencies.length)
+        //await runCommands("Install dependencies", [`sudo apt install -y ${service.dependencies.join(' ')}`])
+      //await runCommands("Run pre-install commands", service.preInstallCmd)
+      await runCommands("Run install commands", service.installCmd, `${process.cwd()}/${service.folder}`)
+      //await runCommands("Start service", [`cd ${service.folder}`,  ...service.runCmd])
       spinnies.success(service.name)
+      console.log(chalk.green("All done !"))
     }))
   } catch (err) {
     console.log(chalk.red('Critical error occured.'))
