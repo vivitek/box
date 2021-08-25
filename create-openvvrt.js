807bdd45d90df8d80663d2ab38766875e5d7470c
Theo Marinho <theo.marinho@epitech.eu>
2021-08-25 19:52:29 +0200

    [fix] remove commentaries

--- create-openvvrt.js
+++ create-openvvrt.js
@@ -7,7 +7,7 @@ const config = require('./config/openvvrt.config.json')
 const log = openSync(`${+new Date()}.log`, "a+")
 const spinnies = new Spinnies()
 
-const runCommands = async (name, commands, execPath = '.') => {
+const runCommands = async (name, commands, execPath) => {
   if (!commands || commands.length === 0)
     return
  await Promise.all(commands.map(async command => {
@@ -39,11 +39,12 @@ const start = async () => {
     console.log(chalk.bold('Initializing services'))
     await Promise.all(config.services.map(async service => {
       spinnies.add(service.name)
-      //if (service.dependencies && service.dependencies.length)
-        //await runCommands("Install dependencies", [`sudo apt install -y ${service.dependencies.join(' ')}`])
-      //await runCommands("Run pre-install commands", service.preInstallCmd)
-      await runCommands("Run install commands", service.installCmd, `${process.cwd()}/${service.folder}`)
-      //await runCommands("Start service", [`cd ${service.folder}`,  ...service.runCmd])
+      const path = `${process.cwd()}/${service.name}`
+      if (service.dependencies && service.dependencies.length)
+        await runCommands("Install dependencies", [`sudo apt install -y ${service.dependencies.join(' ')}`])
+      await runCommands("Run pre-install commands", service.preInstallCmd)
+      await runCommands("Run install commands", service.installCmd, path)
+      await runCommands("Start service", service.runCmd, path)
       spinnies.success(service.name)
       console.log(chalk.green("All done !"))
     }))
