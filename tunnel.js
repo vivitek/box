4ea9fdde9bf85cb016a1026fb701462bc7f4ab0f
Matteo Gassend <matteo.gassend@epitech.eu>
2021-10-13 11:06:09 +0200

    [wip] runs at least until env creation

--- tunnel.js
+++ tunnel.js
@@ -1,45 +1,41 @@
 const { default: axios } = require("axios");
 const config = require("./config/openvvrt.config.json");
+const execa = require("execa");
 const Spinnies = require("spinnies");
 const redis = require("redis");
 const spinnies = new Spinnies();
-
+const os = require("os");
+const chalk = require("chalk");
+const { writeSync, copyFileSync, openSync } = require("fs");
 const initSsh = async () => {
-  const sshKey = await execa.command(
-    "ssh-keygen -t rsa -f ~/.ssh/id_tunnel -N ''"
+  await execa.command(
+    `ssh-keygen -t rsa -f ${os.homedir()}/.ssh/id_tunnel -N ''`
   );
-  console.log(chalk.green(`Generated ssh key: ${sshKey.stdout}`));
 };
 
 const getUuid = async () => {
   const response = await axios.get(`${config.tunnel.server}/uuid`);
-  const json = await response.json();
-  return json.uuid;
+  return response.data.data.uuid;
 };
 
 //retrieve port from remote server api
 const getPort = async () => {
   const response = await axios.get(`${config.tunnel.server}/port`);
-  const json = await response.json();
-  return json.port;
+  return response.data.data.port;
 };
 
 const sendSshKey = async () => {
-  const sshKey = await execa.command("cat ~/.ssh/id_tunnel.pub");
+  const sshKey = await execa.command(`cat ${os.homedir()}/.ssh/id_tunnel.pub`);
+  console.log("got ssh key");
   const data = {
-    key: sshKey.stdout,
+    ssh: sshKey.stdout,
   };
   try {
-    const response = await axios.get(`${config.tunnel.server}/ssh`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Accept: "application/json",
-      },
-      body: JSON.stringify(data),
-    });
-    const json = await response.json();
-    if (json.status === "success") {
+    const response = await axios.post(
+      `${config.tunnel.server}/api/tunnels`,
+      data
+    );
+    if (response.data.status === "success") {
       console.log(chalk.green(`Ssh key sent to server`));
     } else {
       console.log(chalk.red(`Could not send ssh key to server`));
@@ -63,19 +59,26 @@ const writeEnvFile = async (port, uuid) => {
 
 const copyService = async () => {
   spinnies.add("Copying service file");
-  const serviceFile = readFileSync("./config/openvivi-tunnel.service");
-  writeFileSync("/etc/systemd/system/openvivi-tunnel.service", serviceFile);
+  copyFileSync(
+    "./config/openvivi.service",
+    "/etc/systemd/system/openvivi.service"
+  );
   spinnies.succeed("Copying service file");
+  spinnies.add("Copying service script");
+  copyFileSync("./config/openvivi-tunnel.sh", "/usr/bin/openvivi-tunnel.sh");
+  spinnies.succeed("Copying service script");
 };
 
 const run = async () => {
-  spinnies, add("Getting device UUID");
+  spinnies.add("Getting device UUID");
   const uuid = await getUuid();
   spinnies.succeed("Getting device UUID");
   spinnies.add("Getting available port");
   const port = await getPort();
   spinnies.succeed("Getting available port");
+  spinnies.add("Creating SSH Key");
   await initSsh();
+  spinnies.succeed("Creating SSH Key");
   await sendSshKey();
   await writeEnvFile(port, uuid);
   await copyService();
@@ -91,3 +94,5 @@ const run = async () => {
 module.exports = {
   run,
 };
+
+run();
