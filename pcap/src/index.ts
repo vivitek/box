78e1aefbd6098b9e894829021eaa4490c2d4e70b
Remi Cabande <remi.cabande@epitech.eu>
2021-10-24 14:08:12 +0200

    [modif] reindent pcap/src/index.ts

--- pcap/src/index.ts
+++ pcap/src/index.ts
@@ -1,5 +1,4 @@
 import * as pcap from "pcap";
-import * as dns from "dns";
 import { exit } from "process";
 import * as amqplib from "amqplib";
 
@@ -7,42 +6,41 @@ let channel: amqplib.Channel;
 
 // RABBITMQ INIT
 const initRabbitMQ = async () => {
-	try {
-		const connection = await amqplib.connect({
-			hostname: "0.0.0.0",
-			username: "vivi",
-			password: "vivitek",
-		});
-		channel = await connection.createChannel();
-		await channel.assertQueue("pcap");
-	} catch (error) {
-		exit(1);
-	}
+  try {
+    const connection = await amqplib.connect({
+      hostname: "0.0.0.0",
+      username: "vivi",
+      password: "vivitek",
+    });
+    channel = await connection.createChannel();
+    await channel.assertQueue("pcap");
+  } catch (error) {
+    exit(1);
+  }
 };
 
 const sendToQueue = async (data) => {
-	await channel.sendToQueue("pcap", Buffer.from(JSON.stringify(data)));
+  await channel.sendToQueue("pcap", Buffer.from(JSON.stringify(data)));
 };
 
 // PCAP INIT
 const initPcap = async () => {
-	const pcap_session: pcap.PcapSession = pcap.createSession(
-		"", // default interface (== all interfaces ?)
-		{ } // default handle (i think it's meh)
-	);
+  const pcap_session: pcap.PcapSession = pcap.createSession(
+    "", // default interface (== all interfaces ?)
+    {} // default handle (i think it's meh)
+  );
 
-	pcap_session.on("packet", (raw_packet) => {
-		const packet = pcap.decode.packet(raw_packet);
-		const pacData = {
-			"len": packet.pcap_header.len,
-			"saddr": packet.payload.payload.saddr.addr.join('.'),
-			"daddr": packet.payload.payload.daddr.addr.join('.')
-		}
-		sendToQueue(pacData)
-	});
+  pcap_session.on("packet", (raw_packet) => {
+    const packet = pcap.decode.packet(raw_packet);
+    const pacData = {
+      "len": packet.pcap_header.len,
+      "saddr": packet.payload.payload.saddr.addr.join('.'),
+      "daddr": packet.payload.payload.daddr.addr.join('.')
+    }
+    sendToQueue(pacData)
+  });
 };
 
 initRabbitMQ().then(() => {
-	initPcap();
+  initPcap();
 });
-
