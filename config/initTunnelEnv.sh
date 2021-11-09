4f1f415c8fe322efa12382f067997de9010c3f65
Matteo Gassend <matteo.gassend@epitech.eu>
2021-11-09 10:38:44 +0100

    [fix] rm -f

--- config/initTunnelEnv.sh
+++ config/initTunnelEnv.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sudo rm /etc/default/openvivi-tunnel
+sudo rm -f /etc/default/openvivi-tunnel
 cat /tmp/openvivi-tunnel | sudo -E tee /etc/default/openvivi-tunnel
 
--- config/initTunnelService.sh
+++ config/initTunnelService.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sudo rm /etc/systemd/system/openvivi.service
+sudo rm -f /etc/systemd/system/openvivi.service
 cat ./config/openvivi.service.template | sed 's@$PWD@'$PWD'@' | sudo -E tee /etc/systemd/system/openvivi.service
