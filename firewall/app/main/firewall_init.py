d8096f3240b1f09111582befb5a885c9907f2d16
Remy Bouvard <remy.bouvard@epitech.eu>
2021-11-11 15:12:57 +0100

    [add] add bandwidth management

--- firewall/app/main/firewall_init.py
+++ firewall/app/main/firewall_init.py
@@ -1,7 +1,9 @@
 from app.main import redis_client
 from app.main.firewall_manager import FWManager
+from app.main.bandwidth.bandwidthManager import Bandwidth
 
 PyNFT = FWManager()
+bandwidth = Bandwidth()
 
 def init_rules():
     IPArray = redis_client.zrange("ipBan", 0, -1)
@@ -13,3 +15,4 @@ def init_rules():
 
 def init_firewall():
     init_rules()
+    bandwidth.init_tc()
