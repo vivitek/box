8701dbb13bff466072c40122a94366fc78dbc339
Remy Bouvard <remy.bouvard@epitech.eu>
2021-11-11 15:12:57 +0100

    [add] add bandwidth management

--- firewall/app/main/controllers/macController.py
+++ firewall/app/main/controllers/macController.py
@@ -4,11 +4,13 @@ from app.main import redis_client
 from app.main.firewall_manager import FWManager
 from app.main.utils.custom_exception import CustomException
 import app.main.utils.validate_form as validateForm
+from app.main.bandwidth.bandwidthManager import Bandwidth
 
 MAC_FORMAT = "^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})|([0-9a-fA-F]{4}\\.[0-9a-fA-F]{4}\\.[0-9a-fA-F]{4})$"
 
 bp = Blueprint('mac', __name__, url_prefix='/mac')
 PyNFT = FWManager()
+bandwidth = Bandwidth()
 
 @bp.route('/ban', methods=['POST'])
 def banMac():
@@ -38,6 +40,29 @@ def unbanMac():
         redis_client.zrem('macBan', address)
         return response, status.HTTP_200_OK
     except CustomException as e:
-        return(e.reason, e.code)
+        return (e.reason, e.code)
     except Exception as e:
         return (str(e), status.HTTP_500_INTERNAL_SERVER_ERROR)
+    
+@bp.route('/limit', methods=['POST'])
+def limitMac():
+    try:
+        address = request.form.get('address')
+        limit = request.form.get('limit')
+        validateForm.validateForm(address, MAC_FORMAT)
+        limitRank = redis_client.zrank('limitMac', limit)
+        limitNumber = redis_client.zcard('limitMac')
+
+        if (limitRank == None):
+            redis_client.zadd('limitMac', {limit: int(limitNumber) + 1})
+            bandwidth.rule_limit(limit, int(limitNumber) + 1)
+            bandwidth.limit_mac(address, int(limitNumber) + 1)
+        else:
+            # bandwidth.rule_limit(limit, limitRank)
+            bandwidth.limit_mac(address, limitRank + 1)
+        return 'Addess has been limited', status.HTTP_200_OK
+    except CustomException as e:
+        return (e.reason, e.code)
+    except Exception as e:
+        return (str(e), status.HTTP_500_INTERNAL_SERVER_ERROR)
+        
