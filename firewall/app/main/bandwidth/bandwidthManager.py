a4c2d5b603d4db1ddd6cc1d8f35a8ba894fd8e1c
Remy Bouvard <remy.bouvard@epitech.eu>
2021-11-27 17:31:08 +0100

    [add] bandwidth

--- firewall/app/main/bandwidth/bandwidthManager.py
+++ firewall/app/main/bandwidth/bandwidthManager.py
@@ -9,8 +9,6 @@ class Bandwidth():
         os.system('tc qdisc add dev wlp58s0 root handle 1: cbq avpkt 1000 bandwidth 20mbit')
     
     def rule_limit(self, number, classId):
-        print(number)
-        print(classId)
         value = os.system('tc class add dev wlp58s0 parent 1: classid 1:%s cbq rate %smbit allot 1500 prio 5 bounded isolated'%(classId, number))
         if (value != 0):
             raise CustomException('Wrong Argument', status.HTTP_500_INTERNAL_SERVER_ERROR)
@@ -19,14 +17,3 @@ class Bandwidth():
         array = address.split(':')
         os.system('tc filter add dev wlp58s0 parent 1: protocol ip prio 5 u32 match u16 0x0800 0xFFFF at -2 match u16 0x%s%s 0xFFFF at -4 match u32 0x%s%s%s%s 0xFFFFFFFF at -8 flowid 1:%s'%(array[4], array[5], array[0], array[1], array[2], array[3], classId))
         os.system('tc filter add dev wlp58s0 parent 1: protocol ip prio 5 u32 match u16 0x0800 0xFFFF at -2 match u32 0x%s%s%s%s 0xFFFFFFFF at -12 match u16 0x%s%s 0xFFFF at -14 flowid 1:%s'%(array[2], array[3], array[4], array[5], array[0], array[1], classId))
-
-
-
-# def main():
-#     bandwidth = Bandwidth()
-#     # bandwidth.init_tc()
-#     bandwidth.rule_limit(5)
-#     # bandwidth.limit_mac('EE:4E:EF:2C:E0:C1')
-
-# if __name__ == '__main__':
-#     main()
