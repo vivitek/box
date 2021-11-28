e42d24d985398e71b7b73b7b6fd75e7ccd42241d
Ubuntu <thmarinho@outlook.fr>
2021-11-28 16:07:03 +0000

    wanna die

--- openvvrt/src/index.ts
+++ openvvrt/src/index.ts
@@ -2,8 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors()
+  const app = await NestFactory.create(AppModule, {cors: true});
+  app.enableCors({
+        "origin": "*",
+        "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+        "preflightContinue": false,
+        "optionsSuccessStatus": 204,
+    })
   await app.listen(3000);
 }
 bootstrap();
--- openvvrt/src/services/services.controller.ts
+++ openvvrt/src/services/services.controller.ts
@@ -1,9 +1,11 @@
-import { Controller, Get, Post, Param, Query } from '@nestjs/common';
+import { Controller, Get, Post, Param, Query, Sse } from '@nestjs/common';
+import { interval, Observable } from 'rxjs';
 import { ServicesService } from './services.service';
+import fs from "fs"
 
 @Controller('/services')
 export class ServicesController {
-  constructor(private service: ServicesService) {}
+  constructor(private service: ServicesService) { }
   @Get('/')
   getAll() {
     return this.service.getAll();
@@ -38,4 +40,31 @@ export class ServicesController {
   logs(@Query('service') service: string, @Query('lines') lines = '32') {
     return this.service.logs(service, lines);
   }
+
+  @Sse('/sse')
+  sse(@Query('service') service: string): Observable<MessageEvent> {
+    const path = `/root/.pm2/logs/${service}-out.log`
+
+    return interval(5000).pipe(() => null);
+
+  }
 }
+
+// {
+  // graphql: {
+    // out: "",
+    // err: ""
+  // }
+  // pcap: {
+    // out: "",
+    // err: ""
+  // }
+  // dhcp: {
+    // out: "",
+    // err: ""
+  // }
+  // openvvrt: {
+    // out: "",
+    // err: ""
+  // }
+// }
