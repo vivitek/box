9f0e3ec1e1c1dbc14466ec8ebd24ae83e6fcee94
Matteo Gassend <matteo.gassend@epitech.eu>
2021-12-01 16:00:02 +0100

    [fix] add shebang to install script

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 git clone https://github.com/vivitek/box.git
 curl -sL https://deb.nodesource.com/setup_14.x | sudo bash -
 wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
